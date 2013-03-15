704e6fe4cc43cc3361b762552915dfc93a2f0674
Erik Dohnberg <erikdohnberg@gmail.com>
2013-03-15 08:59:45 -0400

    Fixed loop issue that prevented bot responses

--- timebot.rb
+++ timebot.rb
@@ -9,12 +9,12 @@ channel = "#Bitmaker"
 
 #List of potential outputs
 time = Time.new
-CurTime = time.now(%I, %p)
-CurWkDay = time.wday(%a)
+CurTime = time.strftime ("%I %M %p")
+CurWkDay = time.strftime ("%A")
 CurDay = time.day
-CurMonth = time.month(%B)
+CurMonth = time.strftime ("%B")
 CurYear = time.year
-CurZone = time.zone(%Z)
+CurZone = time.strftime ("%Z")
 
 irc_server = TCPSocket.open(server, port)
 
@@ -23,39 +23,39 @@ irc_server.puts "NICK #{nick}"
 irc_server.puts "JOIN #{channel}"
 irc_server.puts "PRIVMSG #{channel} :If you'd like to know the time, date, year, etc. Just ask me!"
 
-until s.eof? do
+until irc_server.eof? do
   msg = irc_server.gets.downcase
   puts msg
-end
 
-#Responses
+	#Responses
 
-if msg.include? "PRIVMSG #{channel} :What time is it?" #|| "PRIVMSG #{channel} :What's the time?"
-	response = "The time is currently #{CurTime}."
-	irc_server.puts "PRIVMSG #{channel} :#{response}"
-end
+	if msg.include? "what time is it?"
+		response = "The time is currently #{CurTime}."
+		irc_server.puts "PRIVMSG #{channel} :#{response}"
+	end
 
-if msg.include? "PRIVMSG #{channel} :What day of the week is it?"
-	response = "It's #{CurWkDay}!"
-	irc_server.puts "PRIVMSG #{channel} :#{response}"
-end
+	if msg.include? "what day of the week is it?"
+		response = "It's #{CurWkDay}!"
+		irc_server.puts "PRIVMSG #{channel} :#{response}"
+	end
 
-if msg.include? "PRIVMSG #{channel} :What day of the month is it?"
-	response = "Today is #{CurMonth} the #{CurDay}!"
-	irc_server.puts "PRIVMSG #{channel} :#{response}"
-end
+	if msg.include? "what day of the month is it?"
+		response = "Today is #{CurMonth} the #{CurDay}!"
+		irc_server.puts "PRIVMSG #{channel} :#{response}"
+	end
 
-if msg.include? "PRIVMSG #{channel} :What month is it?"
-	response = "It's #{CurMonth}!"
-	irc_server.puts "PRIVMSG #{channel} :#{response}"
-end
+	if msg.include? "what month is it?"
+		response = "It's #{CurMonth}!"
+		irc_server.puts "PRIVMSG #{channel} :#{response}"
+	end
 
-if msg.include? "PRIVMSG #{channel} :What year is it?"
-	response = "You must be tired today... It's #{CurYear}!"
-	irc_server.puts "PRIVMSG #{channel} :#{response}"
-end
+	if msg.include? "what year is it?"
+		response = "You must be tired today... It's #{CurYear}!"
+		irc_server.puts "PRIVMSG #{channel} :#{response}"
+	end
 
-if msg.include? "PRIVMSG #{channel} :What time zone am I in?"
-	response = "#{CurZone}"
-	irc_server.puts "PRIVMSG #{channel} :#{response}"
+	if msg.include? "what time zone am i in?"
+		response = "#{CurZone}"
+		irc_server.puts "PRIVMSG #{channel} :#{response}"
+	end
 end
