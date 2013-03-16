ecaddb8402963790c5d78465d77080a97d2b1bfe
Erik Dohnberg <erikdohnberg@gmail.com>
2013-03-16 17:04:42 -0400

    Organized code and added comments

--- timebot.rb
+++ timebot.rb
@@ -6,13 +6,24 @@ server = "irc.ipocalypse.net"
 port = "6667"
 nick = "TimeBot647"
 channel = "#Bitmaker"
-greeting_prefix = "privmsg #bitmaker :"
+# Required prefix in order for bot to know to pay attention to msg
+greeting_prefix = "PRIVMSG #bitmaker :"
 # Words the bot will be looking for
 keywords = ['time', 'day', 'week', 'month', 'year', 'zone']
 
+# TimeBot accesses server defined above and tells it information
+# about itself
+irc_server = TCPSocket.open(server, port)
+irc_server.puts "USER timebot 0 * TimeBot"
+irc_server.puts "NICK #{nick}"
+irc_server.puts "JOIN #{channel}"
+irc_server.puts "PRIVMSG #{channel} :If you'd like to know the time, date, year, etc. Just ask me!"
 
-# List of potential time outputs
+# Method to be accessed from the loop in order to retrieve up-to-date
+# time information and the appropriate response
 def current_time
+
+	# List of potential time outputs
 	time = Time.new
 	@CurTime = time.inspect
 	@CurWkDay = time.strftime ("%A")
@@ -20,30 +31,19 @@ def current_time
 	@CurMonth = time.strftime ("%B")
 	@CurYear = time.year
 	@CurZone = time.strftime ("%Z")
-	# Hash that the bot will respond with given the key's are included 
-# in a message
-@responses = 
-	{['time'] => "The time is currently #{@CurTime}.",
-	['day'] => "Today is the #{@CurDay} (A #{@CurWkDay})",
-	['day', 'week'] => "It's #{@CurWkDay}",
-	['day', 'month'] => "Today is #{@CurMonth} the #{@CurDay}!",
-	['month'] => "It's #{@CurMonth}!",
-	['year'] => "You must be tired today... It's #{@CurYear}!",
-	['zone'] => "#{@CurZone}"
-}
-end
 	
-
-
-irc_server = TCPSocket.open(server, port)
-
-irc_server.puts "USER timebot 0 * TimeBot"
-irc_server.puts "NICK #{nick}"
-irc_server.puts "JOIN #{channel}"
-irc_server.puts "PRIVMSG #{channel} :If you'd like to know the time, date, year, etc. Just ask me!"
-
-# Returns true if the msg includes the given words
-# Usage: includesWords("PRIVMSG: What is the time?", ["day", "week"])
+	# Hash that the bot will respond with given the key's are included 
+	# in a message
+	@responses = 
+		{['time'] => "The time is currently #{@CurTime}.",
+		['day'] => "Today is the #{@CurDay} (A #{@CurWkDay})",
+		['day', 'week'] => "It's #{@CurWkDay}",
+		['day', 'month'] => "Today is #{@CurMonth} the #{@CurDay}!",
+		['month'] => "It's #{@CurMonth}!",
+		['year'] => "You must be tired today... It's #{@CurYear}!",
+		['zone'] => "#{@CurZone}"
+	}
+end
 
 until irc_server.eof? do
   msg = irc_server.gets.downcase
@@ -52,18 +52,22 @@ until irc_server.eof? do
 	# Empty array to be used when the msg word is matched
 	matched_keywords = []
 	
-	has_word = false
+	# Statement used to determine if a message has keyword 
+	has_keyword = false
 
-	# The process of matching the msg word to the keywords
+	# The looped process of matching the msg word to the keywords
 	keywords.each do |word|
 		if msg.include? word
 			matched_keywords << word
-			has_word = true
+			has_keyword = true
 		end
 	end
 
-	if has_word and msg.include? greeting_prefix
-		current_time	
+	# Statement only accesses current_time method if has_keyword
+	# and the greeting prefix are both returning true
+	if has_keyword and msg.include? greeting_prefix
+		current_time
+		# TimeBot response to user's questions about the time
 		irc_server.puts "PRIVMSG #{channel} :#{@responses[matched_keywords]}"
 	end
 
